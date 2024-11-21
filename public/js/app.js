2b40e224f621919c3109c33588a53ef40333ab52
JoSantana0005 <joseasantana05@gmail.com>
2024-11-21 00:10:12 -0400

    comienzo el diseño de la ventana modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,30 @@
+let pagina = 1
+const right_arrow = document.getElementById("Right--Arrow");
+const left_arrow = document.getElementById("Left--Arrow");
+const Ventana = document.getElementById("Ventana--info");
+right_arrow.addEventListener("click",()=>{
+    try{
+        pagina++
+        obtener_peliculas();
+    }catch(error){
+        console.log(error)
+    }
+})
+left_arrow.addEventListener("click",()=>{
+    try{
+        if(pagina > 1){
+            pagina--
+            obtener_peliculas();
+        }
+    }catch(error){
+        console.log(error)
+    }
+})
+
 
 const obtener_peliculas = async() =>{
     try{
-        const respuesta = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a123512ad7d1eb8f1ff144501c87ec1a")
+        const respuesta = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a123512ad7d1eb8f1ff144501c87ec1a&language=es-ES&page=&page="+pagina)
         console.log(respuesta)
         let peliculas = ''
         if (respuesta.status == 200){
@@ -15,6 +38,14 @@ const obtener_peliculas = async() =>{
             </div>`
             });
             document.getElementById("Movie--Container").innerHTML = peliculas
+            const Movie = document.querySelectorAll(".Movie")
+            Movie.forEach((element,index) =>{
+                element.addEventListener("click",()=>{
+                    console.log(datos.results[index]);
+                    Ventana.showModal();
+                })
+            })
+
         }else if(respuesta.status == 404){
             console.log("No se encontraron resultados")
         }
